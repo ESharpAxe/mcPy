ac381b09fa94c2d49615259a704a4738d9804134
ESharpAxe <eg@pobox.com>
2016-01-30 00:29:28 -0800

    update

--- setTilePos.py
+++ setTilePos.py
@@ -1,5 +1,10 @@
 
 #ESharpAxe 20160117a
 from mcpi.minecraft import Minecraft
+HOUSE_OFFSET = 1
+
 mc = Minecraft.create()
-mc.player.setTilePos(2222,111,3333)
+
+player_pos = mc.player.getTilePos()
+
+
